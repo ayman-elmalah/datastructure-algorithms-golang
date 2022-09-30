d53260b6a7bab6795671af49aafdb0fe450ba14f
Ayman Elmalah <aymanelmalah77@gmail.com>
2022-09-30 17:25:07 +0200

    Selection sort

--- selection_sort/main.go
+++ selection_sort/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	array := []int{5, 2, 0, 8, 3}
 
-	fmt.Println(selectionSort(array))
+	fmt.Println(selectionSort(array)) // Output : [0 2 3 5 8]
 }
 
+// Big O is O(n^2)
 func selectionSort(array []int) []int {
 	for i := 0; i < len(array); i++ {
 		smallestIndex := i

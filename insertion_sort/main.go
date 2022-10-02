665e53c01bd37eae326e31779d2d0bb0032c5a98
Ayman Elmalah <aymanelmalah77@gmail.com>
2022-10-02 21:32:12 +0200

    Add insertion sort

--- insertion_sort/main.go
+++ insertion_sort/main.go
@@ -10,10 +10,8 @@ func main() {
 
 func insertionSort(array []int) []int {
 	for i := 1; i < len(array); i++ {
-		for j := i; j > 0; j-- {
-			if array[j-1] > array[j] {
-				array[j-1], array[j] = array[j], array[j-1]
-			}
+		for j := i; j > 0 && array[j-1] > array[j]; j-- {
+			array[j-1], array[j] = array[j], array[j-1]
 		}
 	}
 

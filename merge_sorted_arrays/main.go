3f748406c67dec1b340173f565021fd5c5c5e810
Ayman Elmalah <aymanelmalah77@gmail.com>
2024-12-10 16:25:59 +0200

    Enhance mergesorted arrays

--- merge_sorted_arrays/main.go
+++ merge_sorted_arrays/main.go
@@ -5,42 +5,33 @@ import (
 )
 
 func main() {
-	fmt.Println(mergeSortedArrays([]int{0, 3, 6, 7}, []int{1, 8, 9})) // Output : [0 1 3 6 7 8 9]
+	fmt.Println(Merge2SortedArrays([]int{0, 3, 6, 7}, []int{1, 8, 9})) // Output : [0 1 3 6 7 8 9]
 }
 
-func mergeSortedArrays(array1 []int, array2 []int) []int {
-	var newArray []int
+func Merge2SortedArrays(arr1 []int, arr2 []int) []int {
+	var result []int
 
 	for {
-		if len(array1) == 0 {
-			newArray = append(newArray, array2...)
+		if len(arr1) == 0 {
+			result = append(result, arr2...)
 		}
 
-		if len(array2) == 0 {
-			newArray = append(newArray, array1...)
+		if len(arr2) == 0 {
+			result = append(result, arr1...)
 		}
 
-		if len(array1) == 0 || len(array2) == 0 {
+		if len(arr1) == 0 || len(arr2) == 0 {
 			break
 		}
 
-		first := array1[0]
-		second := array2[0]
-
-		if first < second {
-			newArray = append(newArray, first)
-			array1 = RemoveFromArrayByIndex(array1, 0)
+		if arr1[0] < arr2[0] {
+			result = append(result, arr1[0])
+			arr1 = arr1[1:]
 		} else {
-			newArray = append(newArray, second)
-			array2 = RemoveFromArrayByIndex(array2, 0)
+			result = append(result, arr2[0])
+			arr2 = arr2[1:]
 		}
 	}
 
-	return newArray
-}
-
-func RemoveFromArrayByIndex(s []int, index int) []int {
-	ret := make([]int, 0)
-	ret = append(ret, s[:index]...)
-	return append(ret, s[index+1:]...)
+	return result
 }
